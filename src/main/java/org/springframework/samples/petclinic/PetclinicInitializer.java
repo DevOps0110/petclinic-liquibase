959f89c72a28f4e64efe047133c7c7a254a11325
DevOps0110 <61135591+DevOps0110@users.noreply.github.com>
2020-03-30 12:52:59 +0530

    Update PetclinicInitializer.java

--- src/main/java/org/springframework/samples/petclinic/PetclinicInitializer.java
+++ src/main/java/org/springframework/samples/petclinic/PetclinicInitializer.java
@@ -50,5 +50,31 @@ public class PetclinicInitializer extends AbstractDispatcherServletInitializer {
      * default jpa Spring profile.
      */
 	private static final String SPRING_PROFILE = "jdbc";
+    
+    @Override
+    protected WebApplicationContext createRootApplicationContext() {
+        XmlWebApplicationContext rootAppContext = new XmlWebApplicationContext();
+        rootAppContext.setConfigLocations("classpath:spring/business-config.xml", "classpath:spring/tools-config.xml");
+        rootAppContext.getEnvironment().setDefaultProfiles(SPRING_PROFILE);
+        return rootAppContext;
+    }
 
+    @Override
+    protected WebApplicationContext createServletApplicationContext() {
+        XmlWebApplicationContext webAppContext = new XmlWebApplicationContext();
+        webAppContext.setConfigLocation("classpath:spring/mvc-core-config.xml");
+        return webAppContext;
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+    @Override
+    protected Filter[] getServletFilters() {
+        // Used to provide the ability to enter Chinese characters inside the Owner Form
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter("UTF-8", true);
+        return new Filter[]{characterEncodingFilter};
+    }
 }
